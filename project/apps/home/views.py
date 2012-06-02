cb74a6fa4e38eba0b2d1d09f126b152ce0ecc03b
Steven Skoczen <steven.skoczen@wk.com>
2012-06-02 13:07:04 -0700

    fixing csrf

--- project/apps/home/views.py
+++ project/apps/home/views.py
@@ -1,5 +1,8 @@
 from annoying.decorators import render_to
+from django.views.decorators.csrf import csrf_exempt
 
+
+@csrf_exempt
 @render_to("home/home.html")
 def home(request):
     return locals()
