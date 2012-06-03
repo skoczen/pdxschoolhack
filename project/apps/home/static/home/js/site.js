6babb8db930cdb4612b0519ee4fd3721e7b03b22
Roy Martin <roy.myemail@gmail.com>
2012-06-02 17:18:18 -0700

    initial push for Facebook 1.0

--- project/apps/home/static/home/js/site.js
+++ project/apps/home/static/home/js/site.js
@@ -1,3 +1,35 @@
-$(function(){
+codeAddress = function(){
+	var address = $('#zipcode_input').val();
+	geocoder.geocode({
+		'address' : address
+	}, function(results, status) {
+		if(status == google.maps.GeocoderStatus.OK) {
+			map.setCenter(results[0].geometry.location);
+			map.setZoom(13);
+		} else {
+			//alert("Geocode was not successful for the following reason: " + status);
+		}
+	});
+}
 
+setSchool = function(school,type){
+	
+	console.log(type)
+	
+	if(type == 'High'){
+		$('#high_school').val(school);
+	}else if (type == 'Middle'){
+		$('#middle_school').val(school);
+	}else if(type == 'Elementary'){
+		$('#elementary_school').val(school);		
+	}
+	
+	return false;
+}
+
+$(function() {
+	$('#zipcode_form').submit(function() {
+		codeAddress();
+		return false;
+	});
 });
