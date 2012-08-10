b1d42b376a38455d6e26b06cde396b4821a1c039
Steven Skoczen <steven@quantumimagery.com>
2012-08-09 20:39:31 -0700

    fixed import error

--- project/apps/my_schools/admin.py
+++ project/apps/my_schools/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from events.models import Event
+from my_schools.models import Person, PersonSchool
 
 # class EmailSubscriptionOptions(admin.ModelAdmin):
 #     list_display = ('email',)
 #     search_fields = ('email',)
 
-admin.site.register(Event)
+admin.site.register(Person)
+admin.site.register(PersonSchool)
