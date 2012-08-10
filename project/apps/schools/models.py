3939bcdd430667c9ec71e2f578ea2460a9ece945
Steven Skoczen <steven@quantumimagery.com>
2012-08-09 21:55:45 -0700

    added emaiil to schools

--- project/apps/schools/models.py
+++ project/apps/schools/models.py
@@ -18,6 +18,7 @@ class School(models.Model):
     city = models.CharField(max_length=255, blank=True, null=True)
     zip_code = models.CharField(max_length=5, blank=True, null=True)
     phone_number = models.CharField(max_length=255, blank=True, null=True)
+    email = models.CharField(max_length=255, blank=True, null=True)
     lat = models.FloatField(blank=True, null=True)
     lon = models.FloatField(blank=True, null=True)
     school_type = models.ForeignKey(SchoolType)
