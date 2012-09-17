bd0ecf8960236176c935d7227c98f5b141c43db9
Steven Skoczen <steven@quantumimagery.com>
2012-09-16 21:21:25 -0700

    safer migration.. ish

--- project/apps/my_schools/migrations/0002_auto_remove_facebook_id.py
+++ project/apps/my_schools/migrations/0002_auto_remove_facebook_id.py
@@ -9,7 +9,10 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
         
         # Adding field 'PersonSchool.person'
-        db.delete_column('my_schools_person', 'facebook_id')
+        try:
+            db.delete_column('my_schools_person', 'facebook_id')
+        except:
+            pass
 
 
     def backwards(self, orm):
